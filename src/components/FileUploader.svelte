dbee4bd0b0702694f61e1e83a8e8d4ded8ae0bbd
Irina Ichim <onlyirina7@gmail.com>
2024-05-13 13:31:16 +0200

    file uploader + inicio global.css

---
+++ src/components/FileUploader.svelte
@@ -0,0 +1,38 @@
+<script>
+// @ts-nocheck
+
+	let files;
+
+	$: if (files) {
+		console.log(files);
+
+		for (const file of files) {
+			console.log(`${file.name}: ${file.size} bytes`);
+		}
+	}
+</script>
+
+<label for="avatar">Subir una imagen:</label>
+<input accept="image/png, image/jpeg" bind:files id="avatar" name="avatar" type="file" />
+
+<label for="many">Subir múltiples archivos de cualquier tipo:</label>
+<input bind:files id="many" multiple type="file" />
+
+{#if files}
+	<h2>Archivos seleccionados:</h2>
+	{#each Array.from(files) as file}
+		<p>{file.name} ({file.size} bytes)</p>
+	{/each}
+{/if}
+
+<style>
+    label {
+        display: block;
+        margin-top: 1em;
+    }
+
+    input {
+        display: block;
+        margin-top: 0.5em;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../global.css'
+    import FileUploader from '../components/FileUploader.svelte';
+  </script>
+  
+  <h1>Galería Pública femCoders Club</h1>
+  
+  <FileUploader />
+  
