930a63696cf181d054e3a9c1fada35d9e69aecd4
Irina Ichim <onlyirina7@gmail.com>
2024-05-25 22:17:39 +0200

    start styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,10 @@
   </script>
   
   <header>
-    <script type="module" src="https://unpkg.com/@splinetool/viewer@1.3.8/build/spline-viewer.js"></script>
-<spline-viewer url="https://prod.spline.design/pEFWQWp9qeV4oCvh/scene.splinecode"></spline-viewer>
-   
     <h1>Galería Pública femCoders Club</h1>
+    <script type="module" src="https://unpkg.com/@splinetool/viewer@1.4.0/build/spline-viewer.js"></script>
+    <spline-viewer url="https://prod.spline.design/pEFWQWp9qeV4oCvh/scene.splinecode"></spline-viewer>
+ 
   </header>
   
   <main>
@@ -17,10 +17,28 @@
   
   <footer>
     
-    <p>&copy; 2021 femCoders Club</p>
+    <p>&copy; 2024 femCoders Club</p>
   </footer>
   
   <style>
-    
+      header {
+    width: 100%;
+    height: 100vh; /* 100% del alto de la pantalla */
+    display: flex;
+    justify-content: center;
+
+    align-items: center;
+    overflow: hidden; /* Para ocultar cualquier contenido que se desborde */
+  }
+
+  spline-viewer {
+    width: 100%;
+    height: 100%;
+  }
+  h1{
+    margin-top:100px;
+    color:#e16b29;
+  }
+  
   </style>
   
