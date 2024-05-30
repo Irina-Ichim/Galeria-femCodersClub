6e7236f97093fccca45f54bbeef6ea59c7345cee
Irina Ichim <onlyirina7@gmail.com>
2024-05-30 20:59:48 +0200

    problemas despliegue + probando railway

--- backend/server.mjs
+++ backend/server.mjs
@@ -201,6 +201,8 @@ app.delete("/delete-image/:nombreImagen", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
